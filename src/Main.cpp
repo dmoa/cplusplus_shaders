498618007f63b49f4ba48537631619fd9cc32cb4
Stan <spammyboy@protonmail.com>
2020-03-30 13:49:07 +0100

    light shader working

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,29 +4,39 @@ int main()
 {
 	PlatformHelper platform;
 
-#if defined(_DEBUG)
-	std::cout << "Hello World!" << std::endl;
-#endif
+	#if defined(_DEBUG)
+		std::cout << "Hello World!" << std::endl;
+	#endif
 
 	sf::RenderWindow window;
 	// in Windows at least, this must be called before creating the window
 	float screenScalingFactor = platform.getScreenScalingFactor(window.getSystemHandle());
 	// Use the screenScalingFactor
-	window.create(sf::VideoMode(200.0f * screenScalingFactor, 200.0f * screenScalingFactor), "SFML works!");
+	window.create(sf::VideoMode(500.0f * screenScalingFactor, 500.0f * screenScalingFactor), "SFML works!");
 	platform.setIcon(window.getSystemHandle());
 
-	sf::Shader shader;
-	if (!shader.loadFromFile("content/shader.vert", sf::Shader::Vertex))
-	{
-		throw "Shaders not available!";
-	}
-
-	sf::CircleShape shape(window.getSize().x / 2);
+	sf::RectangleShape shape;
+	shape.setSize(sf::Vector2f(500, 500));
 	shape.setFillColor(sf::Color::White);
 
 	sf::Texture shapeTexture;
-	shapeTexture.loadFromFile("content/sfml.png");
+	shapeTexture.loadFromFile("content/bg.png");
 	shape.setTexture(&shapeTexture);
+
+	sf::Shader shader;
+	if (!shader.loadFromFile("content/shader.frag", sf::Shader::Fragment))
+	{
+		throw "Shaders not available!";
+	}
+	shader.setUniform("screen", sf::Glsl::Vec2(500.0f, 500.0f));
+	shader.setUniform("texture", shapeTexture);
+	shader.setUniform("num_lights", 2);
+	shader.setUniform("lights[0].position", sf::Glsl::Vec2(275.0f, 250.0f));
+	shader.setUniform("lights[0].diffuse", sf::Glsl::Vec3(1.0, 1.0, 1.0));
+	shader.setUniform("lights[0].power", 1.0f);
+	shader.setUniform("lights[1].position", sf::Glsl::Vec2(150.0f, 0.0f));
+	shader.setUniform("lights[1].diffuse", sf::Glsl::Vec3(1.0, 1.0, 1.0));
+	shader.setUniform("lights[1].power", 5.0f);
 
 	sf::Event event;
 
